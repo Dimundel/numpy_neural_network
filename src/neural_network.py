4563d7de2ae0f69df29775c4ad6114d2b004b71f
Dimundel <deenixie@gmail.com>
2025-01-24 22:53:51 +0300

    add ReLU activation layer class

--- src/neural_network.py
+++ src/neural_network.py
@@ -7,3 +7,10 @@ class Layer:
     
     def forward(self, input):
         return np.matmul(self.weights, input) + self.bias
+
+class ReLU:
+    def __init__(self):
+        pass
+    
+    def forward(self, input):
+        return np.maximum(0, input)
