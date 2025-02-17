fix backpropagation for Linear Layer

src/neural_network.py
@@ -8,8 +8,8 @@ class Layer:
     def forward(self, input):
         return np.matmul(input, self.weights) + self.bias
     
-    def back(self, forward_gradient):
-        return forward_gradient*self.weights.transpose()
+    def back(self, forward_value, forward_gradient):
+        return np.matmul(forward_value.transpose(), forward_gradient)
 
 class ReLU:
     def __init__(self):